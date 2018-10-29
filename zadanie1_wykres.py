5e35aa8bc95205fa07ec3633ffdf0fcc9c20f1cd
Piotr Glazer <piotr.glazer@hotmail.com>
2018-10-29 17:46:43 +0100

    Zadanie 1 wykres

--- zadanie1_wykres.py
+++ zadanie1_wykres.py
@@ -2,10 +2,10 @@ from pylab import *
 
 a = int(input("Funkcja a*x-b. Podaj a:"))
 b = int(input("Funkcja a*x-b. Podaj b:"))
-x = range(-10, 11)
+x = arange(-10, 11)
 y = []
 for i in x:
-    y.append(a * i + b)
+    y.append(a * i - b)
 plot(x, y)
 title('Wykres f(x) = a*x - b')
 grid(True)
